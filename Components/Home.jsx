0f2faebfa2c56b4b0f44433daac8537e90f3487a
UkkashAhmad <ahmadukkash47@gmail.com>
2023-12-07 00:15:25 +0500

    Giving module

---
+++ Components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import abc from './Style.module.css'
+
+function Home() {
+  return (
+    <div className={abc.container}>Home</div>
+  )
+}
+
+export default Home
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from '../Components/Home'
+import About from '../Components/About'
+import Service from '../Components/Service'
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <About/>
+      <Service/>
+    </div>
+  )
+}
+
+export default App
